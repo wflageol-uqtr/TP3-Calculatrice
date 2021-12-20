10a603e4707f919c87b51badcb36442a0c3bb0f2
William Flageol <wflageol@gmail.com>
2021-12-20 15:37:22 -0500

    Tests utilisés pour la correction.

--- CalculatriceTests/CalculatorTest.cs
+++ CalculatriceTests/CalculatorTest.cs
@@ -19,83 +19,83 @@ namespace CalculatriceTests
         [TestMethod]
         public void CalculateSimpleString()
         {
-            Assert.AreEqual(5, calculator.Calculate("2 + 3"));
+            Assert.AreEqual(11, calculator.Calculate("7 + 4"));
         }
 
         [TestMethod]
         public void CalculateLongString()
         {
-            Assert.AreEqual(-2, calculator.Calculate("2 * 4 + 3 - 15 / 2"));
+            Assert.AreEqual(2, calculator.Calculate("3 * 5 - 7 + 2 / 5"));
         }
 
         // Facultatif, pour points bonus!
         [TestMethod]
         public void CalculateComplexString()
         {
-            Assert.AreEqual(16, calculator.Calculate("2 + (5 - 3) * (16 / 4)"));
+            Assert.AreEqual(8, calculator.Calculate("3 * (7 - 5) + (10 / 5)"));
         }
 
         [TestMethod]
         public void CalculateSimpleExpression()
         {
             // Arrange
-            var builder = new ExpressionBuilder(2);
-            builder.Add(3);
+            var builder = new ExpressionBuilder(7);
+            builder.Add(4);
             var expression = builder.Build();
 
             // Act
             int result = calculator.Calculate(expression);
 
             // Assert
-            Assert.AreEqual(5, result);
+            Assert.AreEqual(11, result);
         }
 
         [TestMethod]
         public void CalculateLongExpression()
         {
             // Arrange
-            var builder = new ExpressionBuilder(2);
-            builder.Multiply(4);
-            builder.Add(3);
-            builder.Subtract(15);
-            builder.Divide(2);
+            var builder = new ExpressionBuilder(3);
+            builder.Multiply(5);
+            builder.Subtract(7);
+            builder.Add(2);
+            builder.Divide(5);
             var expr = builder.Build();
 
             // Act 
             int result = calculator.Calculate(expr);
 
             // Assert
-            Assert.AreEqual(-2, result);
+            Assert.AreEqual(2, result);
         }
 
         [TestMethod]
         public void CalculateComplexExpression()
         {
             // Arrange
-            var subtractBuilder = new ExpressionBuilder(5);
-            subtractBuilder.Subtract(3);
+            var subtractBuilder = new ExpressionBuilder(7);
+            subtractBuilder.Subtract(5);
 
-            var divideBuilder = new ExpressionBuilder(16);
-            divideBuilder.Divide(4);
+            var divideBuilder = new ExpressionBuilder(10);
+            divideBuilder.Divide(5);
 
-            var builder = new ExpressionBuilder(2);
-            builder.Add(subtractBuilder);
-            builder.Multiply(divideBuilder);
+            var builder = new ExpressionBuilder(3);
+            builder.Multiply(subtractBuilder);
+            builder.Add(divideBuilder);
             var expr = builder.Build();
 
             // Act
             int result = calculator.Calculate(expr);
 
             // Assert
-            Assert.AreEqual(16, result);
+            Assert.AreEqual(8, result);
         }
 
         [TestMethod]
         public void VisitCalculateSimple()
         {
             // Arrange
-            var builder = new ExpressionBuilder(2);
-            builder.Add(3);
+            var builder = new ExpressionBuilder(7);
+            builder.Add(4);
 
             var visitor = new CalculateVisitor();
             var expr = builder.Build();
@@ -104,19 +104,19 @@ namespace CalculatriceTests
             calculator.Visit(expr, visitor);
 
             // Assert
-            Assert.AreEqual(5, visitor.Result);
+            Assert.AreEqual(11, visitor.Result);
         }
 
         [TestMethod]
         public void VisitCalculateLong()
         {
             // Arrange
-            var builder = new ExpressionBuilder(2);
-            builder.Multiply(4);
-            builder.Add(3);
-            builder.Subtract(15);
-            builder.Divide(2);
-            
+            var builder = new ExpressionBuilder(3);
+            builder.Multiply(5);
+            builder.Subtract(7);
+            builder.Add(2);
+            builder.Divide(5);
+
             var visitor = new CalculateVisitor();
             var expr = builder.Build();
 
@@ -124,22 +124,22 @@ namespace CalculatriceTests
             calculator.Visit(expr, visitor);
 
             // Assert
-            Assert.AreEqual(-2, visitor.Result);
+            Assert.AreEqual(2, visitor.Result);
         }
 
         [TestMethod]
         public void VisitCalculateComplex()
         {
             // Arrange
-            var subtractBuilder = new ExpressionBuilder(5);
-            subtractBuilder.Subtract(3);
+            var subtractBuilder = new ExpressionBuilder(7);
+            subtractBuilder.Subtract(5);
 
-            var divideBuilder = new ExpressionBuilder(16);
-            divideBuilder.Divide(4);
+            var divideBuilder = new ExpressionBuilder(10);
+            divideBuilder.Divide(5);
 
-            var builder = new ExpressionBuilder(2);
-            builder.Add(subtractBuilder);
-            builder.Multiply(divideBuilder);
+            var builder = new ExpressionBuilder(3);
+            builder.Multiply(subtractBuilder);
+            builder.Add(divideBuilder);
 
             var visitor = new CalculateVisitor();
             var expr = builder.Build();
@@ -148,15 +148,15 @@ namespace CalculatriceTests
             calculator.Visit(expr, visitor);
 
             // Assert
-            Assert.AreEqual(16, visitor.Result);
+            Assert.AreEqual(8, visitor.Result);
         }
 
         [TestMethod]
         public void VisitPrintSimple()
         {
             // Arrange
-            var builder = new ExpressionBuilder(2);
-            builder.Add(3);
+            var builder = new ExpressionBuilder(7);
+            builder.Add(4);
 
             var visitor = new PrintVisitor();
             var expr = builder.Build();
@@ -165,18 +165,18 @@ namespace CalculatriceTests
             calculator.Visit(expr, visitor);
 
             // Assert
-            Assert.AreEqual("2 + 3", visitor.ToString());
+            Assert.AreEqual("7 + 4", visitor.ToString());
         }
 
         [TestMethod]
         public void PrintCalculateLong()
         {
             // Arrange
-            var builder = new ExpressionBuilder(2);
-            builder.Multiply(4);
-            builder.Add(3);
-            builder.Subtract(15);
-            builder.Divide(2);
+            var builder = new ExpressionBuilder(3);
+            builder.Multiply(5);
+            builder.Subtract(7);
+            builder.Add(2);
+            builder.Divide(5);
 
             var visitor = new PrintVisitor();
             var expr = builder.Build();
@@ -185,22 +185,22 @@ namespace CalculatriceTests
             calculator.Visit(expr, visitor);
 
             // Assert
-            Assert.AreEqual("2 * 4 + 3 - 15 / 2", visitor.ToString());
+            Assert.AreEqual("3 * 5 - 7 + 2 / 5", visitor.ToString());
         }
 
         [TestMethod]
         public void PrintCalculateComplex()
         {
             // Arrange
-            var subtractBuilder = new ExpressionBuilder(5);
-            subtractBuilder.Subtract(3);
+            var subtractBuilder = new ExpressionBuilder(7);
+            subtractBuilder.Subtract(5);
 
-            var divideBuilder = new ExpressionBuilder(16);
-            divideBuilder.Divide(4);
+            var divideBuilder = new ExpressionBuilder(10);
+            divideBuilder.Divide(5);
 
-            var builder = new ExpressionBuilder(2);
-            builder.Add(subtractBuilder);
-            builder.Multiply(divideBuilder);
+            var builder = new ExpressionBuilder(3);
+            builder.Multiply(subtractBuilder);
+            builder.Add(divideBuilder);
 
             var visitor = new PrintVisitor();
             var expr = builder.Build();
@@ -209,7 +209,7 @@ namespace CalculatriceTests
             calculator.Visit(expr, visitor);
 
             // Assert
-            Assert.AreEqual("2 + (5 - 3) * (16 / 4)", visitor.ToString());
+            Assert.AreEqual("3 * (7 - 5) + (10 / 5)", visitor.ToString());
         }
     }
 }
